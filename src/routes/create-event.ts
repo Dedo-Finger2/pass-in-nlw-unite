362cf2cd04d0437a1dc74ca8d64a6c7a97b4c5fb
Greg <antonioimportant@gmail.com>
2024-06-12 09:20:46 -0300

    feat: add create-event route

---
+++ src/routes/create-event.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "./../config/prisma";
+import { randomUUID } from "node:crypto";
+
+export async function createEvent(app: FastifyInstance) {
+  app.post("/events", async (request, reply) => {
+    const requestBodySchema = z.object({
+      title: z.string().min(4),
+      details: z.string().nullable(),
+      maximumAttendees: z.number().int().positive().nullable()
+    });
+
+    try {
+      const { title, details, maximumAttendees } = requestBodySchema.parse(request.body);
+      
+      const event = await prisma.event.create({
+        data: {
+          id: randomUUID(),
+          title,
+          details,
+          maximumAttendees,
+          slug: new Date().toISOString(),
+        }
+      });
+
+      return reply.status(201).send({
+        error: false,
+        success: true,
+        code: 201,
+        message: { eventId: event.id }
+      });
+    } catch (error) {
+      console.error(error);
+      
+      return reply.status(500).send(
+        { 
+          error: true,
+          success: false,
+          code: 500,
+          message: "Internal Server Error."
+        }
+      );
+    }
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import fastify from "fastify";
 
 const app = fastify();
+const baseUrl = "/api/v1";
+
+import { createEvent } from "./routes/create-event";
+
+app.register(createEvent, { prefix: baseUrl });
 
 app.listen({ port: 3333 }, () => {
   console.log("🚀 Server running...");
