c46f03e963df480f86c29a7e93e42b542d0dee00
Greg <antonioimportant@gmail.com>
2024-06-12 16:06:06 -0300

    feat: add get-event-attendees route

---
+++ src/routes/get-event-attendees.ts
@@ -0,0 +1,108 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../config/prisma";
+
+export async function getEventAttendees(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/events/:eventId/attendees", {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          pageIndex: z.string().nullish().default('0').transform(Number),
+          query: z.string().nullish(),
+        }),
+        response: {
+          200: z.object({
+            error: z.boolean(),
+            success: z.boolean(),
+            code: z.number().int().positive(),
+            message: z.object({ attendees: z.array(z.object({
+              id: z.number(),
+              name: z.string(),
+              email: z.string().email(),
+              createdAt: z.date(),
+              checkedInAt: z.date().nullable(),
+            })) 
+          }),
+          }),
+          400: z.object({
+            error: z.boolean(),
+            success: z.boolean(),
+            code: z.number().int().positive(),
+            message: z.string(),
+          }),
+          500: z.object({
+            error: z.boolean(),
+            success: z.boolean(),
+            code: z.number().int().positive(),
+            message: z.string(),
+          }),
+        }
+      }
+    }, async (request, reply) => {
+      try {
+        const { eventId } = request.params;
+        const { pageIndex, query } = request.query;
+
+        const attendees = await prisma.attendee.findMany({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            checkIn: {
+              select: {
+                createdAt: true,
+              }
+            }
+          },
+          where: query ? {
+            eventId,
+            name: {
+              contains: query,
+            }
+          } : {
+            eventId,
+          },
+          take: 10,
+          skip: pageIndex * 10,
+          orderBy: {
+            createdAt: "desc",
+          },
+        });
+
+        return reply.status(200).send({
+          error: false,
+          success: true,
+          code: 200,
+          message: {
+            attendees: attendees.map(attendee => {
+              return {
+                id: attendee.id,
+                name: attendee.name,
+                email: attendee.email,
+                createdAt: attendee.createdAt,
+                checkedInAt: attendee.checkIn?.createdAt ?? null,
+              }
+            })
+          },
+        })
+
+      } catch (error) {
+        console.error(error);
+        
+        return reply.status(500).send(
+          { 
+            error: true,
+            success: false,
+            code: 500,
+            message: "Internal Server Error."
+          }
+        );
+      }
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,14 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 app.register(createEvent, { prefix: baseUrl });
 app.register(registerForEvent, { prefix: baseUrl });
 app.register(getEvent, { prefix: baseUrl });
 app.register(getAttendeeBadge, { prefix: baseUrl });
 app.register(checkIn, { prefix: baseUrl });
+app.register(getEventAttendees, { prefix: baseUrl });
 
 app.listen({ port: 3333 }, () => {
   console.log("🚀 Server running...");
