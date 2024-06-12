c035070162816894f5554698e1d8413897b3109d
Greg <antonioimportant@gmail.com>
2024-06-12 17:45:22 -0300

    feat: add badRequestError annd configure errorHandler

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,7 +1,33 @@
 import { FastifyInstance } from "fastify"
+import { ZodError } from "zod";
+import { BadRequest } from "../routes/_errors/bad-request";
 
 type FastifyErrorHanlder = FastifyInstance['errorHandler'];
 
 export const errorHandler: FastifyErrorHanlder = (error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      error: true,
+      success: false,
+      code: 500,
+      message: "Error during validation.",
+      errors: error.flatten().fieldErrors,  
+    });
+  }
+
+  if (error instanceof BadRequest) {
+    return reply.status(400).send({
+      error: true,
+      success: false,
+      code: 400,
+      message: error.message,
+    })
+  }
   
+  return reply.status(500).send({
+    error: true,
+    success: false,
+    code: 500,
+    message: "Internal Server Error, try again later.",
+  });
 }
